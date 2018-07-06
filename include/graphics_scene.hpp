8278a479b967c69b31aa6b9aca28a758d45cbf0c
Mike Whitney <mikeswhitney33@gmail.com>
2018-07-06 09:53:57 -0600

    added support for macos in the graphics scene

--- include/graphics_scene.hpp
+++ include/graphics_scene.hpp
@@ -14,6 +14,10 @@ public:
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+        #ifdef __APPLE__
+            glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        #endif
+
         window = glfwCreateWindow(width, height, title, NULL, NULL);
         if(window == NULL) {
             std::cout << "Failed to create GLFW window" << std::endl;
